b0b5b791f0cb13958247b7b29aa890f53d7073ce
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-02-28 12:04:06 -0600

    Sumandos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,24 @@ namespace SobrecargaMetodos_G2_2022_II
             this.label2 = new System.Windows.Forms.Label();
             this.lbSuma = new System.Windows.Forms.Label();
             this.btnSuma = new System.Windows.Forms.Button();
+            this.txtbOperando12 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbOpernado22 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbOperando23 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbSuma2 = new System.Windows.Forms.Label();
+            this.btnSuma2 = new System.Windows.Forms.Button();
+            this.txtbOperando13 = new System.Windows.Forms.TextBox();
+            this.txtbOperando32 = new System.Windows.Forms.TextBox();
+            this.txtbOperando33 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtbOperando43 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbSuma3 = new System.Windows.Forms.Label();
+            this.btnSuma3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtbOperando1
@@ -69,32 +87,191 @@ namespace SobrecargaMetodos_G2_2022_II
             // 
             // lbSuma
             // 
-            this.lbSuma.Location = new System.Drawing.Point(662, 136);
+            this.lbSuma.Location = new System.Drawing.Point(587, 136);
             this.lbSuma.Name = "lbSuma";
-            this.lbSuma.Size = new System.Drawing.Size(70, 25);
+            this.lbSuma.Size = new System.Drawing.Size(176, 22);
             this.lbSuma.TabIndex = 0;
             this.lbSuma.Text = "0";
             // 
             // btnSuma
             // 
-            this.btnSuma.Location = new System.Drawing.Point(197, 233);
+            this.btnSuma.Location = new System.Drawing.Point(907, 116);
             this.btnSuma.Name = "btnSuma";
-            this.btnSuma.Size = new System.Drawing.Size(209, 80);
+            this.btnSuma.Size = new System.Drawing.Size(209, 48);
             this.btnSuma.TabIndex = 1;
             this.btnSuma.Text = "Sumar";
             this.btnSuma.UseVisualStyleBackColor = true;
             this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
             // 
+            // txtbOperando12
+            // 
+            this.txtbOperando12.Location = new System.Drawing.Point(92, 225);
+            this.txtbOperando12.Name = "txtbOperando12";
+            this.txtbOperando12.Size = new System.Drawing.Size(78, 31);
+            this.txtbOperando12.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(205, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "+";
+            // 
+            // txtbOpernado22
+            // 
+            this.txtbOpernado22.Location = new System.Drawing.Point(265, 222);
+            this.txtbOpernado22.Name = "txtbOpernado22";
+            this.txtbOpernado22.Size = new System.Drawing.Size(78, 31);
+            this.txtbOpernado22.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(378, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 28);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "+";
+            // 
+            // txtbOperando23
+            // 
+            this.txtbOperando23.Location = new System.Drawing.Point(437, 222);
+            this.txtbOperando23.Name = "txtbOperando23";
+            this.txtbOperando23.Size = new System.Drawing.Size(78, 31);
+            this.txtbOperando23.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(547, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 28);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "=";
+            // 
+            // lbSuma2
+            // 
+            this.lbSuma2.Location = new System.Drawing.Point(583, 225);
+            this.lbSuma2.Name = "lbSuma2";
+            this.lbSuma2.Size = new System.Drawing.Size(74, 25);
+            this.lbSuma2.TabIndex = 0;
+            this.lbSuma2.Text = "0";
+            // 
+            // btnSuma2
+            // 
+            this.btnSuma2.Location = new System.Drawing.Point(907, 217);
+            this.btnSuma2.Name = "btnSuma2";
+            this.btnSuma2.Size = new System.Drawing.Size(209, 46);
+            this.btnSuma2.TabIndex = 1;
+            this.btnSuma2.Text = "Sumar";
+            this.btnSuma2.UseVisualStyleBackColor = true;
+            this.btnSuma2.Click += new System.EventHandler(this.btnSuma2_Click);
+            // 
+            // txtbOperando13
+            // 
+            this.txtbOperando13.Location = new System.Drawing.Point(92, 316);
+            this.txtbOperando13.Name = "txtbOperando13";
+            this.txtbOperando13.Size = new System.Drawing.Size(78, 31);
+            this.txtbOperando13.TabIndex = 0;
+            // 
+            // txtbOperando32
+            // 
+            this.txtbOperando32.Location = new System.Drawing.Point(265, 313);
+            this.txtbOperando32.Name = "txtbOperando32";
+            this.txtbOperando32.Size = new System.Drawing.Size(78, 31);
+            this.txtbOperando32.TabIndex = 0;
+            // 
+            // txtbOperando33
+            // 
+            this.txtbOperando33.Location = new System.Drawing.Point(437, 313);
+            this.txtbOperando33.Name = "txtbOperando33";
+            this.txtbOperando33.Size = new System.Drawing.Size(78, 31);
+            this.txtbOperando33.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(205, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "+";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(378, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 28);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "+";
+            // 
+            // txtbOperando43
+            // 
+            this.txtbOperando43.Location = new System.Drawing.Point(592, 313);
+            this.txtbOperando43.Name = "txtbOperando43";
+            this.txtbOperando43.Size = new System.Drawing.Size(78, 31);
+            this.txtbOperando43.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(533, 316);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 28);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "+";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(717, 313);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 28);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "=";
+            // 
+            // lbSuma3
+            // 
+            this.lbSuma3.Location = new System.Drawing.Point(753, 316);
+            this.lbSuma3.Name = "lbSuma3";
+            this.lbSuma3.Size = new System.Drawing.Size(74, 25);
+            this.lbSuma3.TabIndex = 0;
+            this.lbSuma3.Text = "0";
+            // 
+            // btnSuma3
+            // 
+            this.btnSuma3.Location = new System.Drawing.Point(907, 295);
+            this.btnSuma3.Name = "btnSuma3";
+            this.btnSuma3.Size = new System.Drawing.Size(209, 46);
+            this.btnSuma3.TabIndex = 1;
+            this.btnSuma3.Text = "Sumar";
+            this.btnSuma3.UseVisualStyleBackColor = true;
+            this.btnSuma3.Click += new System.EventHandler(this.btnSuma3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 543);
+            this.ClientSize = new System.Drawing.Size(1392, 540);
+            this.Controls.Add(this.btnSuma3);
+            this.Controls.Add(this.btnSuma2);
             this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.lbSuma3);
+            this.Controls.Add(this.lbSuma2);
             this.Controls.Add(this.lbSuma);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtbOperando2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbOperando43);
+            this.Controls.Add(this.txtbOperando33);
+            this.Controls.Add(this.txtbOperando23);
+            this.Controls.Add(this.txtbOperando32);
+            this.Controls.Add(this.txtbOpernado22);
+            this.Controls.Add(this.txtbOperando13);
+            this.Controls.Add(this.txtbOperando12);
             this.Controls.Add(this.txtbOperando1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -111,6 +288,24 @@ namespace SobrecargaMetodos_G2_2022_II
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lbSuma;
         private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.TextBox txtbOperando12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbOpernado22;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbOperando23;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbSuma2;
+        private System.Windows.Forms.Button btnSuma2;
+        private System.Windows.Forms.TextBox txtbOperando13;
+        private System.Windows.Forms.TextBox txtbOperando32;
+        private System.Windows.Forms.TextBox txtbOperando33;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtbOperando43;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbSuma3;
+        private System.Windows.Forms.Button btnSuma3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,14 +17,55 @@ namespace SobrecargaMetodos_G2_2022_II
         {
             InitializeComponent();
         }
-
+        #region Eventos
         private void btnSuma_Click(object sender, EventArgs e)
         {
             float operando1 =float.Parse( txtbOperando1.Text);
             float operando2 = float.Parse(txtbOperando2.Text);
-            suma = operando1 + operando2;
+            /* suma = operando1 + operando2;
+             lbSuma.Text = suma.ToString();
+            */
+            Sumar(operando1, operando2);
+
+        }
+
+        private void btnSuma2_Click(object sender, EventArgs e)
+        {
+            float operando1 = float.Parse(txtbOperando12.Text);
+            float operando2 = float.Parse(txtbOpernado22.Text);
+            float operando3 = float.Parse(txtbOperando23.Text);
+            Sumar(operando1, operando2, operando3);
+
+        }
+        private void btnSuma3_Click(object sender, EventArgs e)
+        {
+            float operando1 = float.Parse(txtbOperando13.Text);
+            float operando2 = float.Parse(txtbOperando32.Text);
+            float operando3 = float.Parse(txtbOperando33.Text);
+            float operando4 = float.Parse(txtbOperando43.Text);
+            Sumar(operando1, operando2, operando3, operando4);
+        }
+        #endregion
+
+
+        #region Métodos Sobrecargados
+        public void Sumar(float sumando1, float sumando2)
+        {
+            suma = sumando1 + sumando2;
             lbSuma.Text = suma.ToString();
 
         }
+        public void Sumar(float sumando1, float sumando2, float sumando3)
+        {
+            suma = sumando1 + sumando2 + sumando3;
+            lbSuma2.Text = suma.ToString();
+        }
+        public void Sumar(float sumando1, float sumando2, float sumando3, float sumando4)
+        {
+            suma = sumando1 + sumando2 + sumando3 + sumando4;
+            lbSuma3.Text = suma.ToString();
+        }
+        #endregion
+
     }
 }
